974ef8efdbf57c484691b5b27a7b686a5d07b6bc
Kevin Oliver <oliver.395@osu.edu>
2021-03-28 12:43:45 -0400

    Can create a new PPM file

--- Assets/Scripts/GeneratePPM.cs
+++ Assets/Scripts/GeneratePPM.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class GeneratePPM : MonoBehaviour
 {
+    public string fileName;
     // Start is called before the first frame update
     void Start()
     {
-        
+        fileName = "raytrace.ppm";
     }
 
     // Update is called once per frame
@@ -19,5 +21,12 @@ public class GeneratePPM : MonoBehaviour
     public void Generate(int size)
     {
         Debug.Log("Generating ppm");
+        FileStream fs = File.Create(fileName);
+        StreamWriter writer = new StreamWriter(fs);
+        writer.Write("P3\n");
+        writer.Write($"{size} {size}\n");
+        writer.Write("255\n");
+        writer.Write("255 255 255 020 222 145 255 255 255\n000 000 000 255 255 255 000 000 000\n255 255 255 000 000 000 255 255 255");
+        writer.Close();
     }
 }
