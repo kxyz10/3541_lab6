0a6d1693822bea4ef26bc43630ba76d2d2236295
Kevin Oliver <oliver.395@osu.edu>
2021-03-28 14:48:15 -0400

    Starting to make the grid

--- Assets/Scripts/GeneratePPM.cs
+++ Assets/Scripts/GeneratePPM.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 public class GeneratePPM : MonoBehaviour
 {
     public string fileName;
+    GameObject camera;
+    float distanceToImageFrame;
     // Start is called before the first frame update
     void Start()
     {
         fileName = "raytrace.ppm";
+        camera = GameObject.Find("Main Camera");
+        distanceToImageFrame = 5.0f;
     }
 
     // Update is called once per frame
@@ -21,12 +25,26 @@ public class GeneratePPM : MonoBehaviour
     public void Generate(int size)
     {
         Debug.Log("Generating ppm");
+
+
+
         FileStream fs = File.Create(fileName);
         StreamWriter writer = new StreamWriter(fs);
+        Vector3[,] pixelCenters = MakePixelChart(size);
         writer.Write("P3\n");
         writer.Write($"{size} {size}\n");
         writer.Write("255\n");
         writer.Write("255 255 255 020 222 145 255 255 255\n000 000 000 255 255 255 000 000 000\n255 255 255 000 000 000 255 255 255");
         writer.Close();
+    }
+
+    public Vector3[,] MakePixelChart(int size)
+    {
+        //Create the 2D array of the image pixels
+        //Values of the array represent the center of the pixel
+        Vector3[,] chart = new Vector3[size, size];
+        Vector3 center = camera.transform.position + camera.transform.forward * distanceToImageFrame;
+        //need to have case for even and odd size
+        return chart;
     }
 }
